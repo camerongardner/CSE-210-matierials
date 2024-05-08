63c2ee063c442a7f7ee4beceb3c173e6e2ba0c44
camerongardner <gar21019@byui.edu>
2024-05-07 20:31:35 -0600

    Completed Learning2 assignments

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Job 
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_company} {_jobTitle} {_startYear}-{_endYear}");
+    }
+}
+// A code template for the category of things known as Person. The
+    // // responsibility of a Person is to hold and display personal information.
+    // public class Person
+    // {
+        // The C# convention is to start member variables with an underscore _
+        // public string _givenName = "";
+        // public string _familyName = "";
+
+        // A special method, called a constructor that is invoked using the  
+        // new keyword followed by the class name and parentheses.
+        // public Person()
+        // {
+        // }
+
+        // A method that displays the person's full name as used in eastern 
+        // countries or <family name, given name>.
+        // public void ShowEasternName()
+        // {
+        //     Console.WriteLine($"{_familyName}, {_givenName}");
+        // }
+
+        // A method that displays the person's full name as used in western 
+        // countries or <given name family name>.
+    //     public void ShowWesternName()
+    //     {
+    //         Console.WriteLine($"{_givenName} {_familyName}");
+    //     }
+    // }
